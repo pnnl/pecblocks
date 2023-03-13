3a2b80aa19ae26c9d427850e0e259fbd6896bff6
McDermott, Thomas E <thomas.mcdermott@pnnl.gov>
2023-03-13 17:23:46 -0400

    improved Lab2 models with nh1=nh2=30

--- examples/hwpv/train_lab2.bat
+++ examples/hwpv/train_lab2.bat
@@ -1,2 +1,3 @@
 rem python pv3_training.py ./lab1/lab1_config.json c:/data/lab1.hdf5
+rem python pv3_training.py ./lab2/lab2_config.json c:/data/lab2.hdf5
 python pv3_metrics.py ./lab2/lab2_config.json c:/data/lab2.hdf5
