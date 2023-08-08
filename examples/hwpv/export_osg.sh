d9146c80351084d7730794f451baae8d55d76e68
temcdrm <Thomas.McDermott@pnnl.gov>
2023-08-08 16:46:22 -0400

    mac osx solution of osg

--- examples/hwpv/export_osg.sh
+++ examples/hwpv/export_osg.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#declare -r DATA_PATH=/media/sf_src/data
+declare -r DATA_PATH=$HOME/Documents/data
 python3 pv3_export.py ./osg4_uvm/osg4_uvm_config.json > osg4_uvm/metrics.txt
-python3 pv3_metrics.py ./osg4_uvm/osg4_uvm_config.json /media/sf_src/data/osg4_vdvq.hdf5 >> osg4_uvm/metrics.txt
+python3 pv3_metrics.py ./osg4_uvm/osg4_uvm_config.json $DATA_PATH/osg4_vdvq.hdf5 >> osg4_uvm/metrics.txt
 
--- examples/hwpv/train_osg.sh
+++ examples/hwpv/train_osg.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-python3 pv3_training.py ./osg4_uvm/osg4_uvm_config.json /media/sf_src/data/osg4_vdvq.hdf5
+#declare -r DATA_PATH=/media/sf_src/data
+declare -r DATA_PATH=$HOME/Documents/data
+python3 pv3_training.py ./osg4_uvm/osg4_uvm_config.json $DATA_PATH/osg4_vdvq.hdf5
