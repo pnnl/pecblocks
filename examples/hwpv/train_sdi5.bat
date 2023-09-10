57f50c7002b2d9bbbcc558bc7c4dbcbd6273d6f1
McDermott, Thomas E <thomas.mcdermott@pnnl.gov>
2023-09-09 23:02:38 -0400

    trained the unity and non-unity power factor models separately for sdi5

--- examples/hwpv/train_sdi5.bat
+++ examples/hwpv/train_sdi5.bat
@@ -1,2 +1,4 @@
-python pv3_training.py ./sdi5/sdi5_config.json c:/data/sdi5.hdf5
+rem python pv3_training.py ./sdi5/sdi5_config.json c:/data/sdi5.hdf5
+python pv3_training.py ./sdi5a/sdi5a_config.json c:/data/sdi5a.hdf5
+python pv3_training.py ./sdi5b/sdi5b_config.json c:/data/sdi5b.hdf5
 
