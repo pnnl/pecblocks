574082ca062fcb6869a533808468c3aca31051c9
McDermott, Thomas E <thomas.mcdermott@pnnl.gov>
2023-09-15 23:21:49 -0400

    meets RMSE on sdi5 with 5000 iterations, not using Vq input, nh1=60, nh2=40

--- examples/hwpv/export_sdi5.bat
+++ examples/hwpv/export_sdi5.bat
@@ -1,7 +1,7 @@
-rem python pv3_export.py ./sdi5/sdi5_config.json > sdi5\metrics.txt
-rem python pv3_metrics.py ./sdi5/sdi5_config.json c:/data/sdi5.hdf5 >> sdi5\metrics.txt
-python pv3_export.py ./sdi5a/sdi5a_config.json > sdi5a\metrics.txt
-python pv3_metrics.py ./sdi5a/sdi5a_config.json c:/data/sdi5a.hdf5 >> sdi5a\metrics.txt
-python pv3_export.py ./sdi5b/sdi5b_config.json > sdi5b\metrics.txt
-python pv3_metrics.py ./sdi5b/sdi5b_config.json c:/data/sdi5b.hdf5 >> sdi5b\metrics.txt
+python pv3_export.py ./sdi5/sdi5_config.json > sdi5\metrics.txt
+python pv3_metrics.py ./sdi5/sdi5_config.json c:/data/sdi5.hdf5 >> sdi5\metrics.txt
+rem python pv3_export.py ./sdi5a/sdi5a_config.json > sdi5a\metrics.txt
+rem python pv3_metrics.py ./sdi5a/sdi5a_config.json c:/data/sdi5a.hdf5 >> sdi5a\metrics.txt
+rem python pv3_export.py ./sdi5b/sdi5b_config.json > sdi5b\metrics.txt
+rem python pv3_metrics.py ./sdi5b/sdi5b_config.json c:/data/sdi5b.hdf5 >> sdi5b\metrics.txt
 
--- examples/hwpv/train_sdi5.bat
+++ examples/hwpv/train_sdi5.bat
@@ -1,4 +1,4 @@
-rem python pv3_training.py ./sdi5/sdi5_config.json c:/data/sdi5.hdf5
-python pv3_training.py ./sdi5a/sdi5a_config.json c:/data/sdi5a.hdf5
-python pv3_training.py ./sdi5b/sdi5b_config.json c:/data/sdi5b.hdf5
+python pv3_training.py ./sdi5/sdi5_config.json c:/data/sdi5.hdf5 > sdi5\log.txt
+rem python pv3_training.py ./sdi5a/sdi5a_config.json c:/data/sdi5a.hdf5
+rem python pv3_training.py ./sdi5b/sdi5b_config.json c:/data/sdi5b.hdf5
 
