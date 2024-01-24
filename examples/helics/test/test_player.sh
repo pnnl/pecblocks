e9292eb07df10e8e9bbc2c05cf3863cbb6f82a6a
temcdrm <t.mcdermott@ieee.org>
2024-01-24 18:38:58 -0500

    working on HELICS test

--- examples/helics/test/test_player.sh
+++ examples/helics/test/test_player.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-(exec helics_broker -f 2 --name=mainbroker --loglevel debug &> broker.log &)
-(exec helics_player helics_player.txt -n player --local --time_units=s --stop 8.000s --loglevel debug &> player.log &)
-(exec helics_recorder --config-file helics_recorder.txt --stop 8.000s --loglevel debug &> recorder.log &)
+(exec helics_broker -f 2 --name=mainbroker --loglevel trace &> broker.log &)
+(exec helics_player helics_player.txt -n player --local --stop 8s --loglevel debug &> player.log &)
+(exec helics_recorder helics_recorder.txt --verbose --stop 8s --loglevel debug &> recorder.log &)
 
