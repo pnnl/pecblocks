eae47acb8b0bbcf392de8bb20ef68ff47f6347e6
temcdrm <t.mcdermott@ieee.org>
2024-05-12 00:14:07 -0400

    set up training Thevenin IIR models

--- examples/hwpv/train_all.bat
+++ examples/hwpv/train_all.bat
@@ -1,7 +1,7 @@
-call train.bat ucf10t1
-call train.bat sdi5t
-call train.bat osg4t
-call train.bat ucf2t
-call train.bat unb3t
-call train.bat bal3t
+call train.bat ucf10t1iir
+call train.bat sdi5tiir
+call train.bat osg4tiir
+call train.bat ucf2tiir
+call train.bat unb3tiir
+call train.bat bal3tiir
 
