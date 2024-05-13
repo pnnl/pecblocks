131470b6fe53c7fa9864ba35a71b7233b9b93351
temcdrm <t.mcdermott@ieee.org>
2024-05-13 08:14:07 -0400

    trained tiir models

--- examples/hwpv/train_all.bat
+++ examples/hwpv/train_all.bat
@@ -1,7 +1,7 @@
-call train.bat ucf10t1iir
+rem call train.bat ucf10t1iir
 call train.bat sdi5tiir
 call train.bat osg4tiir
 call train.bat ucf2tiir
-call train.bat unb3tiir
+rem call train.bat unb3tiir
 call train.bat bal3tiir
 
