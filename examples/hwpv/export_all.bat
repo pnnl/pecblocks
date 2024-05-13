96201312151d8f34f171d0d1779f8f0069e5eb83
temcdrm <t.mcdermott@ieee.org>
2024-05-13 19:37:23 -0400

    trained thev iir models; only ucf10 benefits from thevenin, others are best with norton

---
+++ examples/hwpv/export_all.bat
@@ -0,0 +1,7 @@
+call export.bat ucf10ciirs2
+call export.bat sdi5
+call export.bat osg4
+call export.bat ucf2
+call export.bat unb3
+call export.bat bal3
+
--- examples/hwpv/train_all.bat
+++ examples/hwpv/train_all.bat
@@ -1,7 +1,7 @@
-rem call train.bat ucf10t1iir
+call train.bat ucf10t1iir
 call train.bat sdi5tiir
 call train.bat osg4tiir
 call train.bat ucf2tiir
-rem call train.bat unb3tiir
+call train.bat unb3tiir
 call train.bat bal3tiir
 
