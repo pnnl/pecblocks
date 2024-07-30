09690a198e828175215d2ea4bd392d659550cb90
temcdrm <t.mcdermott@ieee.org>
2024-07-30 06:20:33 -0400

    train ucf4 variants without sigma optimization

--- examples/hwpv/export_all.bat
+++ examples/hwpv/export_all.bat
@@ -5,7 +5,9 @@ call export.bat sdi5
 call export.bat ucf2
 call export.bat ucf3
 call export.bat ucf4n
+call export.bat ucf4nz
 call export.bat ucf4t
+call export.bat ucf4tz
 call export.bat unb3
 
 
--- examples/hwpv/train_all.bat
+++ examples/hwpv/train_all.bat
@@ -4,7 +4,9 @@ call train.bat sdi5
 call train.bat ucf2
 call train.bat ucf3
 call train.bat ucf4n
+call train.bat ucf4nz
 call train.bat ucf4t
+call train.bat ucf4tz
 rem Save the longest training runs for last
 call train.bat unb3
 call train.bat bal3
